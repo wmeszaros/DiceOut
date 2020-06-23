6630f3dc3ebaaa201ed4a112ae5644db91ea530c
William Meszaros <wmeszaros@gamil.com>
2020-06-22 17:36:09 -0700

    Three dice with images for each, showing result after button push.

--- app/src/main/java/com/example/diceout/MainActivity.kt
+++ app/src/main/java/com/example/diceout/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.diceout
 
+import android.graphics.drawable.Drawable
+import android.media.Image
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -8,8 +11,11 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import java.io.IOException
+import java.io.InputStream
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +25,11 @@ class MainActivity : AppCompatActivity() {
     var score: Int = 0
     lateinit var rand: Random
     var die1: Int = 0
+    var die2: Int = 0
+    var die3: Int = 0
+
+    lateinit var dice: ArrayList<Int>
+    lateinit var diceImageViews: ArrayList<ImageView>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +49,16 @@ class MainActivity : AppCompatActivity() {
 
         rand = Random
 
+        dice = ArrayList()
+        var die1Image = findViewById<ImageView>(R.id.die1Image)
+        var die2Image = findViewById<ImageView>(R.id.die2Image)
+        var die3Image = findViewById<ImageView>(R.id.die3Image)
+
+        diceImageViews = ArrayList<ImageView>()
+        diceImageViews.add(die1Image)
+        diceImageViews.add(die2Image)
+        diceImageViews.add(die3Image)
+
         //Create greeting
         Toast.makeText(applicationContext, "Welcome to DiceOut!", Toast.LENGTH_SHORT).show()
 
@@ -46,7 +67,27 @@ class MainActivity : AppCompatActivity() {
     public fun rollDice(v: View){
         rollResult.setText("Clicked")
         die1 = rand.nextInt(6) + 1
-        val msg = "You rolled a $die1"
+        die2 = rand.nextInt(6) + 1
+        die3 = rand.nextInt(6) + 1
+
+        dice.clear()
+        dice.add(die1)
+        dice.add(die2)
+        dice.add(die3)
+
+        for (dieOfSet in 0..2) {
+            val imageName = "die_" + dice.get(dieOfSet) + ".png"
+            try {
+                val stream = applicationContext.assets.open(imageName)
+                val drawable = Drawable.createFromStream(stream, "")
+                diceImageViews.get(dieOfSet).setImageDrawable(drawable)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+
+
+        val msg = "You rolled a $die1 and a $die2 and a $die3"
         rollResult.setText(msg)
 
 //        var num: Int = rand.nextInt(6) + 1
